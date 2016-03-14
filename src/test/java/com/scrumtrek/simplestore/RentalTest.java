e899a8ac13987338ccbb77f5c48d4094bd952c58
lmikhailov <lmikhailov@yandex.ru>
2016-03-14 19:02:08 +0300

    Test commit

--- src/test/java/com/scrumtrek/simplestore/RentalTest.java
+++ src/test/java/com/scrumtrek/simplestore/RentalTest.java
@@ -66,5 +66,5 @@ public class RentalTest {
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
     }
-    
+     
 }
