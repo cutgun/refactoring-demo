94d0c3949a9f0b4c95354373e0d5d857ea1f6cfd
cutgun <cutgun@gmail.com>
2016-03-14 15:02:33 +0300

    add tests new

---
+++ src/test/java/com/scrumtrek/simplestore/MovieTest.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.scrumtrek.simplestore;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Алексей
+ */
+public class MovieTest {
+    
+    public MovieTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getPriceCode method, of class Movie.
+     */
+    @Test
+    public void testGetPriceCode() {
+        System.out.println("getPriceCode");
+        Movie instance = null;
+        PriceCodes expResult = null;
+        PriceCodes result = instance.getPriceCode();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setPriceCode method, of class Movie.
+     */
+    @Test
+    public void testSetPriceCode() {
+        System.out.println("setPriceCode");
+        PriceCodes value = null;
+        Movie instance = null;
+        instance.setPriceCode(value);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getTitle method, of class Movie.
+     */
+    @Test
+    public void testGetTitle() {
+        System.out.println("getTitle");
+        Movie instance = null;
+        String expResult = "";
+        String result = instance.getTitle();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
